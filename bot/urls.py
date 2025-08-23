294cce41fc03cb61962e628711f9218cf7f38983
XAM30ID <Valiev.Khamza@yandex.ru>
2025-08-23 22:36:44 +0300

    fixed urls

--- bot/urls.py
+++ bot/urls.py
@@ -9,7 +9,7 @@ app_name = 'bot'
 
 urlpatterns = [
     path(settings.BOT_TOKEN, views.index, name="index"),
-    path('bot', views.set_webhook, name="set_webhook"),
+    path('bot/', views.set_webhook, name="set_webhook"),
     path("bot/status/", views.status, name="status"),
     path("payment-info/", views.payment_info, name="payment_info"),
 ]
