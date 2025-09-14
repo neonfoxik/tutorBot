8fd2493ca9c0b2e0d5d292a06037fc69dbe87903
Ilia_m <124691729+neonfoxik@users.noreply.github.com>
2025-09-14 22:00:35 +0300

    Update __init__.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -3,6 +3,6 @@ from .common import (
 )
 from .registration import start_registration, handle_registration_message, handle_education_choice, handle_course_or_class_choice
 from .payments import (
-    payment_menu, start_payment, select_payment_month, 
-    confirm_payment, payment_history, notify_payment_success
+    payment_menu, start_payment, select_payment_method, select_payment_month, 
+    select_balance_payment_month, confirm_payment, payment_history, notify_payment_success
 )
